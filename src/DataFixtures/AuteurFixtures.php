7d3b5106b9a8b2f04080c76bd84b7a3d2be5fb0d
Paul Gravinese <gravinese.paul@orange.fr>
2022-09-28 11:53:57 +0200

    add Auteur

---
+++ src/DataFixtures/AuteurFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Auteur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AuteurFixtures extends Fixture
+{
+    private SluggerInterface $slugger;
+
+    /**
+     * @param SluggerInterface $slugger
+     */
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create("fr_FR");
+
+        for ($i=0; $i<=8;$i++){
+            $auteur = new Auteur();
+            $auteur->setNom($faker->);
+            $auteur->getPrenom($faker->);
+            $auteur->setPseudo($faker->);
+
+            $this->addReference("auteur".$i,$auteur);
+
+            $manager->persist($auteur);
+        }
+
+        $manager->flush();
+    }
+}

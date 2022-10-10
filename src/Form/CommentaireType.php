95d9b0544175d75c9f2922fea0a8921b55ac5e05
pgrvs <gravinese.paul@orange.fr>
2022-10-10 20:40:11 +0200

    travail maison modif commentaireType

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Commentaire;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,8 @@ class CommentaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contenu')
-            ->add('createdAt')
-            ->add('article')
-            ->add('auteur')
+            ->add('contenu', TextType::class)
+            ->add('auteur', TextType::class)
         ;
     }
 
